11d7e5617f1275b298116eae89ad3f675ccd0d98
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2021-12-07 12:44:41 +0700

    basic group controller

---
+++ NET5ChatAppServerAPI/Controllers/GroupsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+using Microsoft.AspNetCore.Mvc;
+using NET5ChatAppServerAPI.Models;
+using NET5ChatAppServerAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace NET5ChatAppServerAPI.Controllers
+{
+	public class GroupsController : BaseApiController
+	{
+		private readonly ChatAppDbContext _context;
+
+		public GroupsController(ChatAppDbContext context)
+		{
+			this._context = context;
+		}
+
+		[HttpGet]
+		public async Task<object> Index()
+		{
+			var records = from o in this._context.Groups
+						  select o;
+
+			if (records.Any() == false)
+			{
+				return "not found";
+			}
+
+			return await records.AsNoTrackingWithIdentityResolution().ToListAsync();	
+		}
+
+		[HttpGet]
+		public async Task<object> Get()
+		{
+			var records = from o in this._context.Groups
+						  select o;
+
+			if (records.Any() == false)
+			{
+				return null;
+			}
+
+			return await records.AsNoTrackingWithIdentityResolution().ToListAsync();
+		}
+	}
+}
