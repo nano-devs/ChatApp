919236291132fdcb7f4693c848dee068269e3d66
MadeYoga <vngla21@gmail.com>
2021-12-07 14:38:53 +0700

    Add RefreshToken model

---
+++ NET5AuthServerAPI/Models/RefreshToken.cs
@@ -0,0 +1,9 @@
+﻿namespace NET5AuthServerAPI.Models
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Token { get; set; }
+    }
+}
