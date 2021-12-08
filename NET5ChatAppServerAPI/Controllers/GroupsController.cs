7de5a442758aa3ebee9ef4e53d44aa42c49867f2
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2021-12-08 15:08:02 +0700

    Add some group functionality
    - Create group
    - Delete group
    - List group
    - Edit group name

--- NET5ChatAppServerAPI/Controllers/GroupsController.cs
+++ NET5ChatAppServerAPI/Controllers/GroupsController.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Linq;
-using System;
+using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
-using NET5ChatAppServerAPI.Models;
-using NET5ChatAppServerAPI.Data;
 using Microsoft.EntityFrameworkCore;
-using System.Threading.Tasks;
+
+using NET5ChatAppServerAPI.Data;
+using NET5ChatAppServerAPI.Models;
 
 namespace NET5ChatAppServerAPI.Controllers
 {
@@ -19,32 +19,189 @@ namespace NET5ChatAppServerAPI.Controllers
 			this._context = context;
 		}
 
+		/// <summary>
+		///		Get all group.
+		/// </summary>
+		/// <returns></returns>
 		[HttpGet]
 		public async Task<object> Index()
 		{
 			var records = from o in this._context.Groups
-						  select o;
+						  select new { o.Id, o.Name };
 
 			if (records.Any() == false)
 			{
 				return "not found";
 			}
 
-			return await records.AsNoTrackingWithIdentityResolution().ToListAsync();	
+			return await records.AsNoTrackingWithIdentityResolution().ToListAsync();
 		}
 
+		/// <summary>
+		///		Get details of specific group.
+		/// </summary>
+		/// <param name="groupId"></param>
+		/// <returns></returns>
 		[HttpGet]
-		public async Task<object> Get()
+		public async Task<object> Details(Guid groupId)
 		{
-			var records = from o in this._context.Groups
-						  select o;
+			var group = await this._context.Groups
+				.AsNoTrackingWithIdentityResolution()
+				.FirstOrDefaultAsync(m => m.Id == groupId);
 
-			if (records.Any() == false)
+			if (group == null)
 			{
-				return null;
+				return this.NotFound();
 			}
 
-			return await records.AsNoTrackingWithIdentityResolution().ToListAsync();
+			return group;
+		}
+
+		/// <summary>
+		///		Create a new group
+		/// </summary>
+		/// <param name="name">Group name.</param>
+		/// <param name="userId">The user id that create the group.</param>
+		/// <returns></returns>
+		[HttpPost]
+		public async Task<Guid> Create(string name, Guid userId)
+		{
+			Groups group = null;
+
+			do
+			{
+				var newGroupId = Guid.NewGuid();
+
+				group = await this._context.Groups
+					.AsNoTrackingWithIdentityResolution()
+					.FirstOrDefaultAsync(o => o.Id.Equals(newGroupId));
+			} while (group != null);
+
+			group = new Groups
+			{
+				Id = Guid.NewGuid(),
+				MemberIds = userId.ToString(),
+				Name = name
+			};
+
+			try
+			{
+				await this._context.AddAsync(group);
+				await this._context.SaveChangesAsync();
+				return group.Id;
+			}
+			catch
+			{
+				return Guid.Empty;
+			}
+		}
+
+		/// <summary>
+		///		Get all member in specific group.
+		/// </summary>
+		/// <param name="groupId"></param>
+		/// <returns></returns>
+		[HttpGet]
+		public async Task<string> GetGroupMember(Guid groupId)
+		{
+			var group = await this.Details(groupId);
+			return ((Groups)group).MemberIds;
+		}
+
+		[HttpPost]
+		public async Task<object> Edit(Guid groupId, string name)
+		{
+			return await this.ChangeName(groupId, name);
+		}
+
+		/// <summary>
+		///		Change group name
+		/// </summary>
+		/// <param name="groupId"></param>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		[HttpPost]
+		public async Task<object> ChangeName(Guid groupId, string name)
+		{
+			var group = await this._context.Groups
+				.AsNoTrackingWithIdentityResolution()
+				.FirstOrDefaultAsync(o => o.Id == groupId);
+
+			if (group != null)
+			{
+				try
+				{
+					group.Name = name;
+					this._context.Groups.Update(group);
+					await this._context.SaveChangesAsync();
+					return this.Ok();
+				}
+				catch
+				{
+					return "Failed to change group name";
+				}
+			}
+			else
+			{
+				return this.NotFound();
+			}
+		}
+
+		/// <summary>
+		///		Delete the group
+		/// </summary>
+		/// <param name="groupId"></param>
+		/// <remarks>All member or user that still in the group automatically removed.</remarks>
+		/// <returns></returns>
+		[HttpPost]
+		public async Task<object> Delete(Guid groupId)
+		{
+			var group = await this._context.Groups
+				.AsNoTrackingWithIdentityResolution()
+				.FirstOrDefaultAsync(o => o.Id == groupId);
+
+			if (group != null)
+			{
+				try
+				{
+					this._context.Remove(group);
+					await this._context.SaveChangesAsync();
+					return groupId;
+				}
+				catch
+				{
+					return "Failed to delete group";
+				}
+			}
+			else
+			{
+				return this.NotFound();
+			}
+		}
+
+		/// <summary>
+		///		Add new member into group
+		/// </summary>
+		/// <param name="groupId"></param>
+		/// <param name="userpId"></param>
+		/// <returns></returns>
+		[HttpPost]
+		public async Task<object> AddMember(Guid groupId, Guid userpId)
+		{
+			return null;
+		}
+
+		/// <summary>
+		///		Remove a user from the group
+		/// </summary>
+		/// <param name="groupId"></param>
+		/// <param name="userpId"></param>
+		/// <returns></returns>
+		[HttpPost]
+		public async Task<object> RemoveMember(Guid groupId, Guid userpId)
+		{
+
+			return null;
 		}
 	}
 }
