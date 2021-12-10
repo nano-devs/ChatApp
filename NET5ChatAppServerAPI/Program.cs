ee142c3eacf07fcba2699bd71c39c49b6174cc2e
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2021-12-10 13:27:23 +0700

    remove kestrel header

--- NET5ChatAppServerAPI/Program.cs
+++ NET5ChatAppServerAPI/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
+using System.Security.Authentication;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Hosting;
@@ -21,6 +23,10 @@ namespace NET5ChatAppServerAPI
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
+					webBuilder.UseKestrel((context, options) =>
+					{
+						options.AddServerHeader = false;
+					});
 					webBuilder.UseStartup<Startup>();
 				});
 	}
