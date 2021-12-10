1d6bcbd96083632a5f79388da00163438608ac0b
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2021-12-10 11:41:02 +0700

    Finalize group api

--- NET5ChatAppServerAPI/Controllers/GroupsController.cs
+++ NET5ChatAppServerAPI/Controllers/GroupsController.cs
@@ -80,13 +80,22 @@ namespace NET5ChatAppServerAPI.Controllers
 			group = new Groups
 			{
 				Id = Guid.NewGuid(),
-				MemberIds = userId.ToString(),
 				Name = name
 			};
-
+			
 			try
 			{
-				await this._context.AddAsync(group);
+				await this._context.Groups.AddAsync(group);
+
+				// create GroupMember after adding into database
+				// to minimize concurency conflict for creating new Group
+				var member = new GroupMember
+				{
+					GroupId = group.Id,
+					UserId = userId
+				};
+
+				await this._context.GroupMembers.AddAsync(member);
 				await this._context.SaveChangesAsync();
 				return group.Id;
 			}
@@ -102,10 +111,14 @@ namespace NET5ChatAppServerAPI.Controllers
 		/// <param name="groupId"></param>
 		/// <returns></returns>
 		[HttpGet]
-		public async Task<string> GetGroupMember(Guid groupId)
+		public async Task<object> GetGroupMember(Guid groupId)
 		{
-			var group = await this.Details(groupId);
-			return ((Groups)group).MemberIds;
+			var records = this._context.GroupMembers
+				.AsNoTrackingWithIdentityResolution()
+				.Where(o => o.GroupId == groupId)
+				.Select(o=>o.UserId);
+
+			return await records.ToListAsync();
 		}
 
 		[HttpPost]
@@ -186,9 +199,28 @@ namespace NET5ChatAppServerAPI.Controllers
 		/// <param name="userpId"></param>
 		/// <returns></returns>
 		[HttpPost]
-		public async Task<object> AddMember(Guid groupId, Guid userpId)
+		public async Task<object> AddMember(Guid groupId, Guid userId)
 		{
-			return null;
+			var exist = this._context.GroupMembers
+				.AsNoTrackingWithIdentityResolution()
+				.Where(o => o.GroupId == groupId && o.UserId == userId);
+
+			if (exist.Any())
+			{
+				return $"{ userId } already a member of group { groupId }";
+			}
+			else
+			{
+				var member = new GroupMember
+				{
+					GroupId = groupId,
+					UserId = userId
+				};
+
+				await this._context.GroupMembers.AddAsync(member);
+				await this._context.SaveChangesAsync();
+				return Ok();
+			}
 		}
 
 		/// <summary>
@@ -198,10 +230,22 @@ namespace NET5ChatAppServerAPI.Controllers
 		/// <param name="userpId"></param>
 		/// <returns></returns>
 		[HttpPost]
-		public async Task<object> RemoveMember(Guid groupId, Guid userpId)
+		public async Task<object> RemoveMember(Guid groupId, Guid userId)
 		{
+			var member = this._context.GroupMembers
+							.AsNoTrackingWithIdentityResolution()
+							.Where(o => o.GroupId == groupId && o.UserId == userId);
 
-			return null;
+			if (member.Any())
+			{
+				this._context.GroupMembers.Remove(await member.FirstOrDefaultAsync());
+				await this._context.SaveChangesAsync();
+				return Ok();
+			}
+			else
+			{
+				return $"{ userId } not a member of group { groupId }";
+			}
 		}
 	}
 }
