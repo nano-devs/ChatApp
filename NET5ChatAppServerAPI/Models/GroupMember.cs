47ebfeb8dff975f6b8557859e3476e8c1e81a833
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2021-12-10 11:39:10 +0700

    Create GroupMember.cs

---
+++ NET5ChatAppServerAPI/Models/GroupMember.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NET5ChatAppServerAPI.Models
+{
+	/// <summary>
+	///		Relationship of user and group
+	/// </summary>
+	public class GroupMember<T> where T : struct
+	{
+		/// <summary>
+		///		User who join the group
+		/// </summary>
+		public T UserId { set; get; }
+
+		/// <summary>
+		///		Group that the user join
+		/// </summary>
+		public T GroupId { set; get; }
+	}
+
+	/// <summary>
+	///		Relationship of user and group
+	/// </summary>
+	public class GroupMember : GroupMember<Guid>
+	{ }
+}
