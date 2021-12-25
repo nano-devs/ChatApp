64bb5957381eb3689d26b1e6519978b759a0380d
MadeYoga <vngla21@gmail.com>
2021-12-25 14:18:59 +0700

    Update dependency injection in startup class

--- NET5AuthServerAPI/Startup.cs
+++ NET5AuthServerAPI/Startup.cs
@@ -1,17 +1,17 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using NET5AuthServerAPI.Models;
 using NET5AuthServerAPI.Services.Authenticators;
-using NET5AuthServerAPI.Services.PasswordHashers;
 using NET5AuthServerAPI.Services.RefreshTokenRepositories;
 using NET5AuthServerAPI.Services.TokenGenerators;
 using NET5AuthServerAPI.Services.TokenValidators;
-using NET5AuthServerAPI.Services.UserRepositories;
 using System.Text;
 
 namespace NET5AuthServerAPI
@@ -31,17 +31,33 @@ namespace NET5AuthServerAPI
         {
             services.AddControllers();
 
+            services.AddIdentityCore<User>(option =>
+            {
+                option.User.RequireUniqueEmail = true;
+
+                // For testing only, remove this for production
+                option.Password.RequireDigit = false;
+                option.Password.RequireNonAlphanumeric = false;
+                option.Password.RequireUppercase = false;
+                option.Password.RequiredLength = 0;
+            }).AddEntityFrameworkStores<AuthenticationDbContext>();
+            services.AddSingleton<IdentityErrorDescriber>();
+
             AuthenticationConfiguration authConfig = new AuthenticationConfiguration();
             configuration.Bind("Authentication", authConfig);
             services.AddSingleton(authConfig);
 
+            string connectionString = configuration.GetConnectionString("sqlite");
+            services.AddDbContext<AuthenticationDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<IRefreshTokenRepository, DatabaseRefreshTokenRepository>();
+            services.AddScoped<Authenticator>();
+
             services.AddSingleton<AccessTokenGenerator>();
             services.AddSingleton<RefreshTokenGenerator>();
             services.AddSingleton<ITokenValidator, RefreshTokenValidator>();
-            services.AddSingleton<IPasswordHasher, BCryptHasher>();
-            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
-            services.AddSingleton<IRefreshTokenRepository, InMemoryRefreshTokenRepository>();
-            services.AddSingleton<Authenticator>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(option =>
             {
