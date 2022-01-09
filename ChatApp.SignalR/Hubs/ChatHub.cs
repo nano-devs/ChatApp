64e409c1ad768bb3ca5e3317db228452b3630c00
MadeYoga <vngla21@gmail.com>
2022-01-09 21:17:49 +0800

    Add signalr base

---
+++ ChatApp.SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿namespace ChatApp.SignalR.Hubs;
+using ChatApp.Api.Models;
+using ChatApp.Api.Services.Repositories;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+public class ChatHub : Hub
+{
+    private readonly IRepository<Group> groupRepository;
+
+    public ChatHub(GroupsRepository groupRepository)
+    {
+        this.groupRepository = groupRepository;
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        if (await groupRepository.FindAsync(x => x.Name == "test") == null)
+        {
+            await groupRepository.AddAsync(new Group { Name = "test" });
+        }
+
+        await Groups.AddToGroupAsync(Context.ConnectionId, "test");
+        await base.OnConnectedAsync();
+    }
+
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.Group("test").SendAsync("ReceiveMessage", user, message);
+    }
+}
