799dce0956531176194ce5000e73195787bf5a5e
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2022-01-12 15:05:02 +0700

    Update group repository

---
+++ ChatApp.Api/Services/Repositories/IGroupsRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatApp.Api.Services.Repositories;
+
+using ChatApp.Api.Data;
+using ChatApp.Api.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+public interface IGroupsRepository : IRepository<Group, int>
+{
+	Task<Group?> GetByUniqueIdAsync(Guid id);
+}
